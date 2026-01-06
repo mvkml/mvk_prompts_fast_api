5663934f03bbc7b0161eda56a99e90edd1c4397b
mvkml <mvkwithmath@gmail.com>
2026-01-06 20:05:28 +0530

    basic mock prompt
    basic mock prompt

---
+++ Prompts/pt/002_PT_API/app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # points to app/
+
+class Settings(BaseSettings):
+    env: str = "development"
+    open_ai_key:str=""
+    app_name: str = "VishAgent API"
+    host: str = "0.0.0.0"
+    port: int = 825
+
+    db_host: str
+    db_port: int
+    db_name: str
+    db_user: str
+    db_password: str
+
+    log_level: str = "INFO"
+
+    class Config:
+        env_file = str(BASE_DIR / ".env.dev")
+
+settings = Settings()
